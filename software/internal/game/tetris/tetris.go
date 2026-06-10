b235775c4bfe6dc8eb211c66e57d608265e2f1d9
louis-bourgault <louis.bourgault2009@gmail.com>
2026-06-10 20:05:17 +0800

    started tetris, still lots to do.

--- software/internal/game/tetris/tetris.go
+++ software/internal/game/tetris/tetris.go
@@ -53,8 +53,117 @@ func (d *TetrisGame) Update(dt float64, input platform.InputSystem, log platform
 	return d
 }
 
+type location struct {
+	x int8
+	y int8
+}
+
+func getNthBit(int uint16, n uint8) bool {
+	return (int & (1 << n)) != 0
+}
+
+func (d *TetrisGame) moveLeft() {
+	var occupiedPositions []location
+	copy := d.currentTetromino.rotations[d.currentRotationIndex]
+
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			if getNthBit(copy, uint8(i*4+j)) {
+				occupiedPositions = append(occupiedPositions, location{
+					x: d.currentTopLeftX + int8(i),
+					y: d.currentTopLeftY + int8(j),
+				})
+			}
+		}
+	}
+	for _, pos := range occupiedPositions {
+		if d.blocks[pos.y*10+pos.x-1] != 0 {
+			return
+		}
+	}
+	d.currentTopLeftX -= 1
+}
+
+func (d *TetrisGame) moveRight() {
+	var occupiedPositions []location
+	copy := d.currentTetromino.rotations[d.currentRotationIndex]
+
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			if getNthBit(copy, uint8(i*4+j)) {
+				occupiedPositions = append(occupiedPositions, location{
+					x: d.currentTopLeftX + int8(i),
+					y: d.currentTopLeftY + int8(j),
+				})
+			}
+		}
+	}
+	for _, pos := range occupiedPositions {
+		if d.blocks[pos.y*10+pos.x+1] != 0 {
+			return
+		}
+	}
+	d.currentTopLeftX += 1
+}
+
+func (d *TetrisGame) moveDown() {
+	var occupiedPositions []location
+	copy := d.currentTetromino.rotations[d.currentRotationIndex]
+
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			if getNthBit(copy, uint8(i*4+j)) {
+				occupiedPositions = append(occupiedPositions, location{
+					x: d.currentTopLeftX + int8(i),
+					y: d.currentTopLeftY + int8(j),
+				})
+			}
+		}
+	}
+	for _, pos := range occupiedPositions {
+		if d.blocks[(pos.y-1)*10+pos.x] != 0 {
+			d.getNewTetromino()
+			return
+		}
+	}
+	d.currentTopLeftY -= 1
+}
+
+//todo: rotation, clearing, game over, main event loop, scoring, basically most of the game.
+
+func (d *TetrisGame) getNewTetromino() {
+	d.currentTetromino = tetronimoes[helpers.RandomInt(0, len(tetronimoes)-1)]
+	d.currentRotationIndex = 0
+	d.currentTopLeftX = 5
+	d.currentTopLeftY = 25
+}
+
 func (d *TetrisGame) Draw(screen platform.Screen) {
 	screen.Fill(0x0000)
 	screen.FillRect(60, 0, 120, 240, 0xd6ba)
+	for i := 30; i > 10; i-- {
+		for j := 0; j < 10; j++ {
+			if d.blocks[i*10+j] != 0 {
+				screen.FillRect(int(j)*12+60, (29-i)*12, 12, 12, d.blocks[i*10+j])
+			}
+		}
+	}
+	var occupiedPositions []location
+	copy := d.currentTetromino.rotations[d.currentRotationIndex]
+
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			if getNthBit(copy, uint8(i*4+j)) {
+				occupiedPositions = append(occupiedPositions, location{
+					x: d.currentTopLeftX + int8(i),
+					y: d.currentTopLeftY + int8(j),
+				})
+			}
+		}
+	}
+	for _, pos := range occupiedPositions {
+		screen.FillRect(int(pos.x)*12+60, (29-int(pos.y))*12, 12, 12, d.currentTetromino.color)
+	}
+
 	screen.Present() //present the screen
 }
