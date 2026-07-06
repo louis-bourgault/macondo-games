faa9b2d8061877c0b53486610cdc12c4823430a3
louis-bourgault <louis.bourgault2009@gmail.com>
2026-07-06 18:46:31 +0800

    feat: mircopython sdk work

---
+++ mp-sdk/input.py
@@ -0,0 +1,36 @@
+from machine import Pin
+
+class Input:
+    def __init__(self):
+        self.pin_a = Pin(4, Pin.IN, Pin.PULL_UP) #enable internal pull-up resistor
+        self.pin_b = Pin(5, Pin.IN, Pin.PULL_UP) 
+        self.dpad_up = Pin(9, Pin.IN, Pin.PULL_UP)
+        self.dpad_left = Pin(10, Pin.IN, Pin.PULL_UP)
+        self.dpad_right = Pin(11, Pin.IN, Pin.PULL_UP)
+        self.dpad_down = Pin(12, Pin.IN, Pin.PULL_UP)
+        self.keysHeld = {
+            "A": False,
+            "B": False,
+            "UP": False,
+            "DOWN": False,
+            "LEFT": False,
+            "RIGHT": False
+        }
+        self.oldKeysHeld = self.keysHeld.copy()  
+    def update(self):
+        self.oldKeysHeld = self.keysHeld.copy() 
+        self.keysHeld["A"] = not self.pin_a.value() #invert the value because of pull-up resistor
+        self.keysHeld["B"] = not self.pin_b.value()
+        self.keysHeld["UP"] = not self.dpad_up.value()
+        self.keysHeld["DOWN"] = not self.dpad_down.value()
+        self.keysHeld["LEFT"] = not self.dpad_left.value()
+        self.keysHeld["RIGHT"] = not self.dpad_right.value()
+
+    def is_pressed(self, key):
+        return self.keysHeld.get(key, False)
+    def was_just_pressed(self, key):
+        return self.keysHeld.get(key, False) and not self.oldKeysHeld.get(key, False)
+    def was_just_released(self, key):
+        return not self.keysHeld.get(key, False) and self.oldKeysHeld.get(key, False)
+
+    
--- mp-sdk/main.py
+++ mp-sdk/main.py
@@ -1,19 +1,19 @@
 from machine import Pin
-import machine
 import time
 import os
-
-import sys
-print(sys.path)
-print(os.listdir())
+print("files included in this run: ", os.listdir())
 
 from display import Display
+from input import Input
 
 print("initialising display")
 display = Display()
+input = Input()
 
+input.update()
 display.update()
 display.fill(0x0000) 
-display.text("Hello, World!", 10, 10, 0xFFFF)
-display.update()
+
+
+
 
