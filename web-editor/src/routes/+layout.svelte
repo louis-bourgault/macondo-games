68617b9da301abdffa2f4ee6036061a51246aa0c
louis-bourgault <louis.bourgault2009@gmail.com>
2026-07-12 10:42:05 +0800

    chore: update favicon and page title

--- web-editor/src/routes/+layout.svelte
+++ web-editor/src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 	import { ModeWatcher } from "mode-watcher";
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><link rel="icon" href={favicon} />
+<title>Editor | Ferretboard</title></svelte:head>
 <ModeWatcher/>
 <div class='h-full w-full'>
 	{@render children()}
